384fc9cb4106c80488629921ebe651fd4e4bd1e8
vunam2000 <vunam722000@gmail.com>
2023-11-26 18:19:32 +0700

    Update strategy pattern

--- src/core/real_estate/realEstateStrategy.py
+++ src/core/real_estate/realEstateStrategy.py
@@ -1,17 +1,6 @@
-from core.real_estate.realEstateWebsite import RealEstateWebsite
-from src.core.website.website import Website
-
 class RealEstateStrategy:
-    website: RealEstateWebsite
-
-    def __init__(self):
-        super().__init__()
-
     def crawlName(self):
-        self.website.crawlName()
+        pass
 
     def crawlPhoneNumber(self):
-        self.website.crawlPhoneNumber()
-
-    def getPageContent(self):
-        self.website.getPageContent()
+        pass
--- src/core/website/website.py
+++ src/core/website/website.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By 
 from core.web_driver.webDriver import WebDriver
 
 
@@ -5,12 +6,19 @@ class Website():
     def __init__(self, url):
         self.url = url
         self.webDriver = WebDriver()
+        self.getPageContent()
 
     def getPageContent(self):
+        print(222)
         self.webDriver.getPageContent(cmd='get', url_crawl=self.url)
 
-    def excuteCrawl(self):
-        pass
-
-    def getElementByClass(self):
-        pass
+    def getElementByClass(self, className):
+        print("111",className)
+        return self.webDriver.driver.find_elements(By.CLASS_NAME, className)
+    
+    def getElementByCssSelector(self, css_selector):
+        print("css_selector",css_selector)
+        f = open("demofile2.html", "a")
+        f.write(self.webDriver.driver.page_source)
+        f.close()
+        return self.webDriver.driver.find_elements(By.CSS_SELECTOR, css_selector)
